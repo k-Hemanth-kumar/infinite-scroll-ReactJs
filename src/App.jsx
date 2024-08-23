b3306fe80a761c4be24e7dbd3f40ca867778fd8c
k-Hemanth-kumar <88437971+k-Hemanth-kumar@users.noreply.github.com>
2024-08-23 13:58:51 +0000

    added loader and data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux';
 import { getData } from './components/actionslice';
+import InfiniteScrollContent from './components/infinite-scrollcontent';
 function App() {
   const [data,setData]=useState([]);
   const[apiQuery,setApiQuery]=useState("");
-  const [noData,setNoData]=useState("");
-  const [error,setError]=useState(false);
-  const store=useSelector((state)=>state.infiniteScrollContent);
-  console.log(store);
-  const dispatch=useDispatch();
-  //console.log(noData,data);
+  const [noData,setNoData]=useState(true);
+  const [error,setError]=useState({flag:false,message:""});
   const handleApiQuery=(e)=>{
     setData([]);
     setNoData(false);
@@ -18,18 +15,6 @@ function App() {
     if(e.target.value.length===0){
       setData([]);
     }
-    //dispatch(getData({apiQuery,pageNumber:1}));
-    getData(apiQuery,1)
-    .then((data)=>{
-      if(!data.docs.length){
-        setNoData(true);
-      }
-      setData((prevData)=>[...prevData,...data.docs])
-    })
-    .catch((error)=>{
-      setError(true);
-      console.log(error);
-    });
   }
   const getData=(apiQuery,pageNumber)=>{
     return new Promise(async (resolve,reject)=>{
@@ -38,15 +23,22 @@ function App() {
           const response=await fetch(`https://openlibrary.org/search.json?q=${apiQuery}&page=${pageNumber}`);
           if (response.ok) {
             const data = await response.json();
-            resolve(data); // Resolve with the data instead of the raw response
+            resolve(); // Resolve with the data instead of the raw response
+            if(!data.docs.length){
+              setNoData(true);
+            }
+            console.log(data)
+            setData((prevData)=>[...prevData,...data.docs]);
           } else {
-            //console.log(`Error: HTTP status ${response.status}`);
-            reject(`Error: HTTP status ${response.status}`);
+            console.log(`Error: HTTP status ${response.status}`);
+            setError({flag:true,message:error});
+            reject();
           }
           
         } catch (error) {
-          //console.log("error fetching the data",error);
-          reject(error);
+          console.log("error fetching the data",error);
+          setError({flag:true,message:error});
+          reject();
         }
       }
     })
@@ -58,7 +50,11 @@ function App() {
         <input type="text" value={apiQuery} name='query' id='query' className='query' 
         placeholder='Enter input to search' onChange={handleApiQuery} />
       </div>
-      <div className='infinite-scroll-content'></div>
+      <div className='infinite-scroll-content'>
+        {error.flag?<p style={{width:'100%',textAlign:'center'}}>Something went wrong. {error.message}</p>:apiQuery!==""&&(
+        <InfiniteScrollContent noData={noData} fetchData={getData} data={data} apiQuery={apiQuery} />
+      )}
+      </div>
     </div>
   )
 
--- src/components/infinite-scrollcontent.jsx
+++ src/components/infinite-scrollcontent.jsx
@@ -1,3 +1,76 @@
-export default function InfiniteScrollContent(){
-    
+import { useEffect, useRef, useState } from "react";
+import { getData } from "./actionslice";
+
+export default function InfiniteScrollContent({ data, noData, fetchData,apiQuery }) {
+    console.log("renderd")
+    const [loading,setLoading]=useState(false);
+    const pageNumber=useRef(1);
+    const observer=useRef(null);
+    useEffect(()=>{
+        const debouncetime=setTimeout(() => {
+            handleData();
+        }, 1000);
+        return ()=>{
+            clearTimeout(debouncetime)
+        }
+    },[apiQuery]);
+
+    const observeLastElement=(node)=>{
+        console.log(node,pageNumber.current);
+        if(loading){
+            return;
+        }
+        if(observer.current){
+            observer.current.disconnect();
+        }
+        observer.current=new IntersectionObserver((entries)=>{
+            console.log(entries);
+            if(entries[0].isIntersecting){
+                pageNumber.current+=1
+                handleData();
+            }
+        },{threshold:1,rootMargin:'-50px'});
+        if(node){
+            observer.current.observe(node);
+        }
+    }
+
+
+    const handleData=()=>{
+        setLoading(true);
+        fetchData(apiQuery,pageNumber.current).finally(()=>{
+            setLoading(false);
+        })
+    }
+    return(<>
+    <div className="infinitescroll-wrapper">
+        {
+            noData?<span className="no-results">No search results found.</span>:(
+                data.map((item,index)=>{
+                    if(index===data.length-1){
+                        return renderListElement(item,index,observeLastElement);
+                    }
+                    return renderListElement(item,index,null);
+                })
+            )
+        }
+    </div>
+        <div className="row loader_row">
+            {loading&&<div className="col-md-12" >
+                <div className="loader" id="loader">
+                    <span></span>
+                    <span>
+                    </span>
+                    <span></span>
+                </div>
+            
+            </div>}
+        </div>
+    </>
+        );
+}
+
+function renderListElement(item,index,ref){
+    //console.log(ref,title,key)
+    return <p className="item" key={index} ref={ref}>{item.title}</p>
 }
